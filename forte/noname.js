1208479ff9064cc63c841b3f3baf51d8d38828a0
Jeon <Jeon@DESKTOP-HR7K5D9>
2016-08-11 15:00:06 +0900

    Commit

---
+++ forte/noname.js
@@ -0,0 +1,10 @@
+/**
+ * http://usejsdoc.org/
+ */
+console.log(Boolean(0));
+console.log(Boolean(NaN));
+console.log(Boolean(''));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean('0'));
+console.log(Boolean('false'));
